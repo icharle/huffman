8b5423cdf14800d84ad416b5e0a0f5c148bb8399
icharle <icharle@live.com>
2018-03-17 12:19:00 +0800

    Initial Commit

---
+++ data/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data
+//
+//  Created by John on 2018/3/17.
+//  Copyright © 2018年 John. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
